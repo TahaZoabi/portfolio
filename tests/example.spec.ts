c5b69988c5110c00a7b5632ffbabbd221e58f776
Taha Zoabi <zoabi03@gmail.com>
2024-12-16 10:53:40 +0200

    basic tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,19 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('http://localhost:5173/')
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  await expect(page).toHaveTitle('Taha Zoabi')
+})
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('header title', async ({ page }) => {
+  await page.goto('http://localhost:5173/')
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+  // Expects page to have a heading visible
+  await expect(
+    page.getByRole('heading', {
+      name: "I'm Taha Zoabi, Software Engineering Student."
+    })
+  ).toBeVisible()
+})
